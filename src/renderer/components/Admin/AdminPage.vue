c4a6827e2e979b210ab77337b8ed9ff0f4d76f19
shengliang <sligxl@163.com>
2020-02-27 19:50:26 +0800

    删除添加管理员的按钮，这个功能已被我精简掉了

--- src/renderer/components/Admin/AdminPage.vue
+++ src/renderer/components/Admin/AdminPage.vue
@@ -4,11 +4,11 @@
 			<el-breadcrumb class="breadcrumb" separator="/">
 				<el-breadcrumb-item>管理员</el-breadcrumb-item>
 			</el-breadcrumb>
-			<div class="operation-nav">
+			<!-- <div class="operation-nav">
 				<router-link to="/dashboard/user">
 					<el-button type="primary" icon="plus">添加管理员</el-button>
 				</router-link>
-			</div>
+			</div> -->
 		</div>
 		<div class="content-main">
 			<div class="form-table-box">
